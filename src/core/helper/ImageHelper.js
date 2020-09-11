03ba1abad67feaa2a870ae41f6e9ff1c3678f986
Emma Paris <aswinajraj@gmail.com>
2020-09-11 09:27:43 +0530

    generic file for formatting images

---
+++ src/core/helper/ImageHelper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ImageHelper = ({ product }) => {
+    const imageurl = product ? product.image
+        : `https://www.pexels.com/photo/candle-holder-and-two-champagne-glasses-2522669/`
+    return (
+        <div className="rounded border border-success p-2">
+            <img src={imageurl}
+                style={{ maxHeight: "100%", maxWidth="100%" }}
+                className="mb-3 rounded"
+                alt="" />
+        </div>
+    )
+}
+
+export default ImageHelper
