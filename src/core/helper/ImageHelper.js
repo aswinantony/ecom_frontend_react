34bfe36e5193e85b33f7e0a597dcfb23a271f569
Emma Paris <aswinajraj@gmail.com>
2020-09-14 10:00:04 +0530

    fixing bugs

--- src/core/helper/ImageHelper.js
+++ src/core/helper/ImageHelper.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const ImageHelper = ({ product }) => {
-    const imageurl = product ? product.image
-        : `https://www.pexels.com/photo/candle-holder-and-two-champagne-glasses-2522669/`
+    const imageurl = product
+        ? product.image
+        : `https://www.pexels.com/photo/candle-holder-and-two-champagne-glasses-2522669/`;
     return (
         <div className="rounded border border-success p-2">
-            <img src={imageurl}
-                style={{ maxHeight: "100%", maxWidth="100%" }}
+            <img
+                src={imageurl}
+                style={{ maxHeight: "100%", maxWidth: "100%" }}
                 className="mb-3 rounded"
-                alt="" />
+                alt=""
+            />
         </div>
-    )
-}
+    );
+};
 
-export default ImageHelper
+export default ImageHelper;
