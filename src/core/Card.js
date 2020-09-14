9fc045449a55b61ecc4a3b7780674087bf1d93f0
Emma Paris <aswinajraj@gmail.com>
2020-09-14 09:58:54 +0530

    adding API info to card

---
+++ src/core/Card.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import ImageHelper from "./helper/ImageHelper";
+
+const Card = ({
+    product,
+    addtoCart = true,
+    removeFromCart = false,
+}) => {
+
+
+    const cartTitle = product ? product.name : "Prod missing"
+    const cartDescription = product ? product.description : "Desc missing"
+    const cartPrice = product ? product.price : "Price missing"
+
+    return (
+        <div className="card text-white bg-dark border border-info ">
+            <div className="card-header lead">(cartTitle)</div>
+            <div className="card-body">
+                <ImageHelper product={product} />
+                <p className="lead bg-success font-weight-normal text-wrap">
+                    {cartDescription}
+                </p>
+                <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
+                <div className="row">
+                    <div className="col-12">
+                        <button
+                            onClick={() => { }}
+                            className="btn btn-block btn-outline-success mt-2 mb-2"
+                        >
+                            Add to Cart
+              </button>
+                    </div>
+                    <div className="col-12">
+                        <button
+                            onClick={() => { }}
+                            className="btn btn-block btn-outline-danger mt-2 mb-2"
+                        >
+                            Remove from cart
+              </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
