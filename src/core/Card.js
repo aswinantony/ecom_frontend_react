e95184ece2c33ba1f20d8db65e4dfd874184bbcf
Emma Paris <aswinajraj@gmail.com>
2020-09-15 07:49:28 +0530

    a small bug fixed

--- src/core/Card.js
+++ src/core/Card.js
@@ -15,7 +15,7 @@ const Card = ({
 
     return (
         <div className="card text-white bg-dark border border-info ">
-            <div className="card-header lead">(cartTitle)</div>
+            <div className="card-header lead">{cartTitle}</div>
             <div className="card-body">
                 <ImageHelper product={product} />
                 <p className="lead bg-success font-weight-normal text-wrap">
