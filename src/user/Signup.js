f1472df15b840a977a038434cf0c5143d5274c76
Emma Paris <aswinajraj@gmail.com>
2020-10-01 09:50:47 +0530

    signup form

---
+++ src/user/Signup.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react"
+import Base from "../core/Base";
+import { Link } from "react-router-dom"
+
+import { signup } from "..auth/helper"
+
+const Signup = () => {
+
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        errr: "",
+        success: false,
+    })
+
+    const { name, email, password, error, success } = values
+
+    const handleChange = name =>
+        (event) => {
+            setValues({ ...values, error: false, [name]: event.target.value });
+        }
+
+    const signUpForm = () => {
+        return (
+            <div className="row">
+                <div className="col-md-6 offset-sm-3 text-left">
+                    <form>
+                        <div className="form-group">
+                            <label className="text-light">Name</label>
+                            <input
+                                className="form-control"
+                                value={name}
+                                onChange={handleChange("name")}
+                                type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input
+                                className="form-control"
+                                value={email}
+                                onChange={handleChange("email")}
+                                type="text" />
+                        </div>
+                        <div className="form-group">
+                            <label className="text-light">Email</label>
+                            <input
+                                className="form-control"
+                                value={password}
+                                onChange={handleChange("password")}
+                                type="password" />
+                        </div>
+                        <button className="btn btn-success btn-block">Submit</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <Base title="Signup Page" description="A Signup for LCO User">
+            {signUpForm()}
+            <p>Test of Signup Page</p>
+        </Base>
+    )
+};
+
+export default Signup;
