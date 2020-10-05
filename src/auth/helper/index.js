d8bc32eb25829f716670dbfb62a3d31e85c34e02
Emma Paris <aswinajraj@gmail.com>
2020-10-05 09:08:26 +0530

    typo

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -1,36 +1,48 @@
-import { API } from "../../backend"
-import { cartEmpty } from "../../core/helper/cartHelper"
+import { API } from "../../backend";
+import { cartEmpty } from "../../core/helper/cartHelper";
 
-export const signup = user => {
+export const signup = (user) => {
     return fetch(`${API}user/`, {
         method: "POST",
         headers: {
             Accept: "application/json",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(user),
     })
-        .then(response => {
+        .then((response) => {
             return response.json();
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
 };
 
-export const signin = user => {
-    const formData = new FormData()
+export const signin = (user) => {
+    const formData = new FormData();
 
     for (const name in user) {
-        formData.append(name, user[name])
+        console.log(user[name]);
+        formData.append(name, user[name]);
+    }
+
+    // const {email, password} = user;
+    // const formData = new FormData();
+    // formData.append('email', email)
+    // formData.append('password', password)
+
+    for (var key of formData.keys()) {
+        console.log("MYKEY: ", key);
     }
 
     return fetch(`${API}user/login/`, {
         method: "POST",
+
         body: formData,
     })
-        .then(response => {
+        .then((response) => {
+            console.log("SUCCESS", response);
             return response.json();
         })
-        .catch(err => console.log(err))
+        .catch((err) => console.log(err));
 };
 
 export const authenticate = (data, next) => {
@@ -42,30 +54,33 @@ export const authenticate = (data, next) => {
 
 export const isAuthenticated = () => {
     if (typeof window == undefined) {
-        return false
+        return false;
     }
     if (localStorage.getItem("jwt")) {
-        return JSON.parse(localStorage.getItem("jwt"))
+        return JSON.parse(localStorage.getItem("jwt"));
+        //TODO: compare JWT with database json token
     } else {
         return false;
     }
 };
 
-export const signout = next => {
+export const signout = (next) => {
     const userId = isAuthenticated() && isAuthenticated().user.id;
 
+    console.log("USERID: ", userId);
+
     if (typeof window !== undefined) {
-        localStorage.removeItem("jwt")
+        localStorage.removeItem("jwt");
         cartEmpty(() => { });
-        next();
+        //next();
 
         return fetch(`${API}user/logout/${userId}`, {
-            method: "GET"
+            method: "GET",
         })
-            .then(response => {
-                console.log("Signout Success")
+            .then((response) => {
+                console.log("Signout success");
                 next();
             })
-            .catch(err => console.log(err));
+            .catch((err) => console.log(err));
     }
-}
+};
